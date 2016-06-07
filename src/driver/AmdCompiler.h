bc5c3f8bc63a23f41dbc50806ad19e2658fe05c7
Nikolay Haustov <nikolay.haustov@amd.com>
2016-06-07 12:55:06 +0300

    Add method descriptions.

--- src/driver/AmdCompiler.h
+++ src/driver/AmdCompiler.h
@@ -7,6 +7,10 @@
 
 namespace amd {
 
+/*
+ * DataType is a kind of input, output or intermediate represenation 
+ * to the compiler.
+ */
 enum DataType {
   DT_CL,
   DT_CL_HEADER,
@@ -18,9 +22,13 @@ enum DataType {
 
 class FileReference;
 class File;
-
 class Compiler;
 
+/*
+ * Data is a container for input, output or intermediate representation.
+ *
+ * id is used for DT_CL_HEADER to specify the header name.
+ */
 class Data {
 private:
   std::string id;
@@ -40,6 +48,9 @@ public:
 
 bool FileExists(const std::string& name);
 
+/*
+ * FileReference is a reference to some file.
+ */
 class FileReference : public Data {
 private:
   std::string name;
@@ -57,6 +68,9 @@ public:
   bool Exists() const;
 };
 
+/*
+ * File is a file that can also be modified.
+ */
 class File : public FileReference {
 private:
   bool readonly;
@@ -72,6 +86,9 @@ public:
   bool Exists() const;
 };
 
+/*
+ * BufferReference provides read-only  access to user-managed buffer.
+ */
 class BufferReference : public Data {
 private:
   const char* ptr;
@@ -90,6 +107,9 @@ public:
   bool ReadOutputFile(File* f) override { assert(false); return false; }
 };
 
+/*
+ * Buffer is a modifiable buffer backed by its own storage.
+ */
 class Buffer : public Data {
 private:
   std::vector<char> buf;
@@ -108,39 +128,122 @@ public:
   bool ReadOutputFile(File* f) override;
 };
 
-class LLVMModule : public Data {
-};
-
+/*
+ * Compiler may be used to invoke different phases OpenCL compiler.
+ *
+ * All Data instances and corresponding resources created by Compiler instance,
+ * including files * and buffers are destroyed this compiler instance is destroyed.
+ * Additionally, as debug information may contain references to intermediate files.
+ *
+ * The lifetime of Compiler instance should be normally same as lifetime
+ * of OpenCL program that invokes it.
+ *
+ * Compiler is not guaranteed to be thread safe.
+ */
 class Compiler {
 public:
   virtual ~Compiler() {}
 
+  /*
+   * Return output of this compiler.
+   */
   virtual std::string Output() = 0;
 
+  /*
+   * Create new FileReference with given type and pointing to file with given name.
+   *
+   * If parent is specified, name is assumed to be relative to parent, otherwise absolute.
+   */
   virtual FileReference* NewFileReference(DataType type, const std::string& name, File* parent = 0) = 0;
 
+  /*
+   * Create new FileReference with given type and pointing to file with given name.
+   *
+   * If parent is specified, name is assumed to be relative to parent, otherwise absolute.
+   *
+   * The file will not be automatically deleted when Compiler instance is destroyed.
+   */
   virtual File* NewFile(DataType type, const std::string& name, File* parent = 0) = 0;
 
+  /*
+   * Create File* pointing to new temporary file name.
+   *
+   * If name is specified, it is used as temporary file name.
+   *
+   * If parent is specified, name is assumed to be relative to parent. Otherwise, it is
+   * under temporary directory associated with this Compiler instance.
+   *
+   * The file will be automatically deleted when Compiler instance is destroyed.
+   */
   virtual File* NewTempFile(DataType type, const std::string& name = "", File* parent = 0) = 0;
 
+  /*
+   * Create File* pointing to new temporary directory.
+   *
+   * If parent is specified, name is assumed to be relative to parent. Otherwise, it is
+   * under temporary directory associated with this Compiler instance.
+   *
+   * The directory will be automatically deleted when Compiler instance is destroyed.
+   */
   virtual File* NewTempDir(File* parent = 0) = 0;
 
+  /*
+   * Create BufferReference.
+   */
   virtual BufferReference* NewBufferReference(DataType type, const char* ptr, size_t size, const std::string& id = "") = 0;
 
+  /*
+   * Create new Buffer (initially empty).
+   */
   virtual Buffer* NewBuffer(DataType type) = 0;
 
+  /*
+   * Compile several inputs to LLVM Bitcode.
+   *
+   * Each input should have one of types DT_CL, DT_CL_HEADER, DT_LLVM_BC, DT_LLVM_LL.
+   * output should have one of types DT_LLVM_BC or DT_LLVM_LL.
+   *
+   * Returns true on success or false on failure.
+   */
   virtual bool CompileToLLVMBitcode(const std::vector<Data*>& inputs, Data* output, const std::vector<std::string>& options) = 0;
 
+  /*
+   * Link several LLVM Bitcode inputs.
+   *
+   * Each input should have one of types DT_LLVM_BC, DT_LLVM_LL.
+   * output should have one of types DT_LLVM_BC, DT_LLVM_LL
+   *
+   * Returns true on success or false on failure.
+   */
   virtual bool LinkLLVMBitcode(const std::vector<Data*>& inputs, Data* output, const std::vector<std::string>& options) = 0;
 
+  /*
+   * Compile several inputs directly to executable object.
+
+   * Each input should have one of types DT_CL, DT_CL_HEADER, DT_LLVM_BC, DT_LLVM_LL.
+   * output should have type DT_EXECUTABLE.
+   *
+   * Returns true on success or false on failure.
+   */
   virtual bool CompileAndLinkExecutable(const std::vector<Data*>& inputs, Data* output, const std::vector<std::string>& options) = 0;
 };
 
+/*
+ * CompilerFactory is used to create Compiler's.
+ *
+ * Normally there is single instance of CompilerFactory.
+ *
+ * CompilerFactory does not own Compiler's that it creates. They should be
+ * destroyed with delete.
+ */
 class CompilerDriver {
 public:
+  /*
+   * Create new instance of OpenCL compiler with AMDGPU backend.
+   */
   Compiler* CreateAMDGPUCompiler(const std::string& llvmBin);
 };
 
 }
 
-#endif
+#endif // AMD_COMPILER_DRIVER_H
